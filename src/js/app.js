3e0bc93366fb22edcb783ba0bb4d261b28c18bfb
Chihiro Urabe <chihiro.urabe@gmail.com>
2015-08-29 02:02:49 +0900

    サムネイルの削除機能を追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,18 @@
 (function(window,undefined){
   var fileupload =  new Fileupload({
     btn: $('#js-thumb-upload__btn'),
+    removeBtn: $('.thumb-upload__remove'),
     renderWrapper: $('#js-thumb-upload'),
     template: '<li class="thumb-upload__list list-unstyled">' +
                   '<a href="#" class="">' +
                     '<img src="" height="80" >'  +
                   '</a>' +
+                  '<div class="js−thumb-remove thumb-upload__remove">' +
+                    '削除する' +
+                  '</div>' +
               '</li>',
-      limit: 5,
+      limit: 3,
+      setLayout: true,
       callback: function(){
         //表示数によって、アップロードボタンのスタイル切り替え
       }
--- src/js/m-fileupload.js
+++ src/js/m-fileupload.js
@@ -4,6 +4,7 @@
     this.postedfile = $('.file-posted');
     $.extend(true,this,_options);
     this.initChangeEvents();
+    this.initRemoveEvent();
   };
   Fileupload.prototype.initChangeEvents = function(){
     var self = this;
@@ -13,6 +14,16 @@
         self.btn.val('');
       };
     });
+  };
+  Fileupload.prototype.initRemoveEvent = function(){
+    var self = this;
+    $(document).on('click',this.removeBtn,function(e){
+      self.removeThumb(e.target);
+    });
+  },
+  Fileupload.prototype.removeThumb = function(_target){
+    $(_target).closest('.thumb-upload__list').remove();
+    this.toggleThumbDisplay();
   };
   Fileupload.prototype.readImageFile = function(_files){
     var self = this;
@@ -28,7 +39,7 @@
           };
           //追加した画像をDOMに追加
           self.renderThumb();
-          self.setThumbLayout();
+          self.setLayout && self.setThumbLayout();
           self.toggleThumbDisplay();
           self.callback && self.callback();
         };
@@ -50,12 +61,14 @@
   Fileupload.prototype.renderThumb = function(){
     this.thumb = $(this.template);
     $(this.thumb).find('img')
-                    .attr('src',this.imageFile.url)
-                    .attr('role','posted-file');
+      .attr('src',this.imageFile.url)
+      .attr('role','posted-file');
     this.renderWrapper.append(this.thumb);
   },
   Fileupload.prototype.toggleThumbDisplay = function(){
-    $(this.btn).css('display',(this.renderWrapper.find('img[role=posted-file]').length >= this.limit) ? 'none' : 'block');
+    var _flg = this.renderWrapper.find('img[role=posted-file]').length >= this.limit;
+    $(this.btn).toggleClass('disabled',_flg);
+    $(this.btn).find('input').prop('disabled',_flg);
   },
   Fileupload.prototype.setThumbLayout = function(){
     var _img = new Image(),
@@ -71,8 +84,8 @@
     this.thumb.find('img').attr('width',(_imgWidth > _imgHeight) ? 'auto' : this.thumb.innerWidth());
 
     //画像センタリング
-    // _thumbLeft = this.thumb.innerWidth()/2 - ((_imgWidth > _imgHeight) ? this.thumb.find('img').innerHeight()/2 : this.thumb.find('img').innerWidth()/2)
-    // this.thumb.find('img').css('margin-left',_thumbLeft + 'px');
+    _thumbLeft = this.thumb.innerWidth()/2 - this.thumb.find('img').innerWidth()/2;
+    this.thumb.find('img').css('margin-left',_thumbLeft + 'px');
   }
 
 
